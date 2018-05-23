85810e37a71ba44ddf95ebcbbbd5e91d0e6647e9
Kamil Samigullin <kamil@samigullin.info>
2018-05-23 14:17:47 +0300

    publish vote mechanic

--- docs/js/main.js
+++ docs/js/main.js
@@ -77,3 +77,86 @@
 
     showMessages();
 }(window.jQuery));
+
+// Vote
+(function ($) {
+    'use strict';
+
+    // const IE >= 11
+
+    const $votes = $('.vote'),
+          $likes = $('#likes');
+
+    if (!$votes.length || !$likes.length) {
+        return;
+    }
+
+    const mirror = {
+        like: 'liked',
+        liked: 'like',
+    };
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie/Simple_document.cookie_framework
+    const cookies = {
+        getItem: function (sKey) {
+            if (!sKey) { return null; }
+            return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+        },
+        setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+            if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
+            var sExpires = "";
+            if (vEnd) {
+                switch (vEnd.constructor) {
+                    case Number:
+                        sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
+                        break;
+                    case String:
+                        sExpires = "; expires=" + vEnd;
+                        break;
+                    case Date:
+                        sExpires = "; expires=" + vEnd.toUTCString();
+                        break;
+                }
+            }
+            document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+            return true;
+        },
+        removeItem: function (sKey, sPath, sDomain) {
+            if (!this.hasItem(sKey)) { return false; }
+            document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
+            return true;
+        },
+        hasItem: function (sKey) {
+            if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
+            return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+        },
+        keys: function () {
+            var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
+            for (var nLen = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) { aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]); }
+            return aKeys;
+        }
+    };
+
+    function swap() {
+        const $to = $(this);
+        if (!mirror.hasOwnProperty($to.data('state'))) {
+            return;
+        }
+        const $from = $likes.children('[data-state="' + mirror[$to.data('state')] + '"]');
+        if (!$from.length) {
+            return;
+        }
+        $to.html($from.html()).data('state', $from.data('state'));
+        cookies.setItem(this.id, $from.data('state'));
+    }
+
+    $votes.removeClass('d-none').click(function () {
+        swap.apply(this);
+    }).each(function () {
+        if (cookies.hasItem(this.id)) {
+            if (cookies.getItem(this.id) !== $(this).data('state')) {
+                swap.apply(this);
+            }
+        }
+    });
+}(window.jQuery));
