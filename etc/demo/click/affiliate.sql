158adf3c98df56af1681d7c159f5302d9873c561
Kamil Samigullin <kamil@samigullin.info>
2018-08-26 12:09:05 +0300

    fix #81: replace designmodo by creative-tim

--- etc/demo/click/affiliate.sql
+++ etc/demo/click/affiliate.sql
@@ -6,7 +6,6 @@ DO $$
   DECLARE themeforest  "link"."id"%TYPE   := '57f2c508-b76e-48a1-ae44-ec59a425ea81';
   DECLARE linode       "link"."id"%TYPE   := '098625d3-c6e3-45d2-a9fe-3102623ab197';
   DECLARE vultr        "link"."id"%TYPE   := '00049bd0-a305-43fc-877a-c3579eaabf75';
-  DECLARE designmodo   "link"."id"%TYPE   := 'ce465427-9de9-46ce-ad49-536c75b33070';
 BEGIN
   INSERT INTO "link" ("user", "id", "name")
   VALUES
@@ -15,8 +14,7 @@ BEGIN
     (demoUser, ecwid, '#1 Free E-commerce Shopping Cart & Online Store Solution - Try Ecwid!'),
     (demoUser, themeforest, 'WordPress Themes & Website Templates from ThemeForest'),
     (demoUser, linode, 'SSD Cloud Hosting & Linux Servers - Linode'),
-    (demoUser, vultr, 'SSD VPS Servers, Cloud Servers and Cloud Hosting by Vultr - Vultr.com'),
-    (demoUser, designmodo, 'Designmodo: Web Design Blog, Website and Email Template Builders')
+    (demoUser, vultr, 'SSD VPS Servers, Cloud Servers and Cloud Hosting by Vultr - Vultr.com')
     ON CONFLICT DO NOTHING;
 
   INSERT INTO "alias" ("link_id", "urn")
@@ -26,8 +24,7 @@ BEGIN
     (ecwid, 'ecwid'),
     (themeforest, 'themeforest'),
     (linode, 'linode'),
-    (vultr, 'vultr'),
-    (designmodo, 'designmodo')
+    (vultr, 'vultr')
     ON CONFLICT DO NOTHING;
 
   INSERT INTO "target" ("link_id", "uri")
@@ -37,8 +34,7 @@ BEGIN
     (ecwid, 'http://open.ecwid.com/l6Q7P'),
     (themeforest, 'https://themeforest.net/?ref=kamilsk'),
     (linode, 'https://www.linode.com/?r=2380225d05c74b69bd7e553a8c763808733fc71f'),
-    (vultr, 'https://www.vultr.com/?ref=7232541'),
-    (designmodo, 'https://designmodo.com/products/?u=22')
+    (vultr, 'https://www.vultr.com/?ref=7232541')
     ON CONFLICT DO NOTHING;
 END;
 $$;
