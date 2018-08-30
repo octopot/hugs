1d3c3f00bd2f80249275c1454e85ec5913ec471c
Kamil Samigullin <kamil@samigullin.info>
2018-08-30 12:10:52 +0300

    issue #83: improve events

--- docs/js/main.js
+++ docs/js/main.js
@@ -129,8 +129,8 @@
     }
 
     var mirror = {
-        like: 'liked',
-        liked: 'like',
+        like: 'unlike',
+        unlike: 'like',
     };
 
     // https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie/Simple_document.cookie_framework
